b9396860dafdbbdf2556385bb9340892de5b267f
EerieAlchemist <66958528+EerieAlchemist@users.noreply.github.com>
2021-08-07 17:03:08 -0400

    github command base

--- src/interactions/GitHubCommand.js
+++ src/interactions/GitHubCommand.js
@@ -1,4 +1,6 @@
 import Discord from "discord.js";
+import fetch from "node-fetch";
+import {log} from "nodemon/lib/utils";
 
 module.exports = {
     name: "interactionCreate",
@@ -9,22 +11,57 @@ module.exports = {
 
         if (interaction.commandName === "github") {
 
-            const content = interaction.options.getString("description");
-            const channel = interaction.options.getChannel("channel");
+            const user = interaction.options.getString("user");
+            // const repository = interaction.options.getString("repository");
 
-            interaction.reply({content: "The poll was successfully published."});
+            await fetch(`https://api.github.com/users/${user}`)
+                .then(res => res.json())
+                .then(async body => {
 
-            const embed = new Discord.MessageEmbed()
-                .setTitle("Server Poll")
-                .setColor("#00e1ff")
-                .setDescription(content)
-                .setFooter(interaction.user.tag,client.user.displayAvatarURL({dynamic: true}))
-                .setTimestamp()
+                    if (body.message) {
+                        return await interaction.reply({content: "The user was not found.", ephemeral: true});
+                    }
 
-            channel.send({embeds: [embed]})
-                .then(async msg => {
-                    await msg.react(emojis.success);
-                    await msg.react(emojis.error);
+                    const {
+                        login, // The GitHub username.
+                        avatar_url, // The account avatar URL.
+                        name, // The account name.
+                        html_url, // The link to the account profile.
+                        public_repos, // The count of public repositories owned by the account.
+                        followers, // The amount of followers the account has.
+                        following, // The amount of accounts the user is following.
+                        location,
+                        company, // The information listed in the "company section" of the user's profile.
+                        created_at, // When the account was created.
+                        bio // The account's biography.
+                    } = body;
+
+                    const embed = new Discord.MessageEmbed()
+                        .setTitle(`${login} - GitHub`)
+                        .setAuthor(`${login}'s GitHub Profile`, avatar_url)
+                        .setURL(html_url)
+                        .setThumbnail(avatar_url)
+                        .setColor("#00e1ff")
+                        .setDescription(`‣ ${bio}` +
+                            `\n‣ Public Repositories: ${public_repos}` +
+                            `\n‣ Followers: ${followers}` +
+                            `\n‣ Following: ${following}`)
+                        .addFields(
+                            {
+                                name: "Company/Organization",
+                                value: company || "None found.",
+                                inline: false
+                            },
+                            {
+                                name: "Account Location",
+                                value: location || "None specified.",
+                                inline: false
+                            }
+                        )
+                        .setFooter(`Account created: ${created_at}`)
+
+                    await interaction.reply({embeds: [embed]})
+
                 });
 
         }
