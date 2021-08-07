5f3fc7213255ec7e0cb18596b3eab4d35c63e1a5
EerieAlchemist <66958528+EerieAlchemist@users.noreply.github.com>
2021-08-07 14:47:05 -0400

    added poll command

---
+++ src/interactions/PollCommand.js
@@ -0,0 +1,39 @@
+import {emojis} from "../resources/config.json";
+import Discord from "discord.js";
+
+module.exports = {
+    name: "interactionCreate",
+    once: true,
+    async execute(interaction, client) {
+
+        if (!interaction.isCommand()) return;
+
+        if (interaction.commandName === "poll") {
+
+            const content = interaction.options.getString("description");
+            const channel = interaction.options.getChannel("channel");
+
+            interaction.reply({content: "The poll was successfully published."});
+
+            const embed = new Discord.MessageEmbed()
+                .setTitle("Server Poll")
+                .setColor("#00e1ff")
+                .setDescription(content)
+                .setFooter(interaction.user.tag,client.user.displayAvatarURL({dynamic: true}))
+                .setTimestamp()
+
+            channel.send({embeds: [embed]})
+                .then(async msg => {
+                    await msg.react(emojis.success);
+                    await msg.react(emojis.error);
+                });
+
+        }
+    }
+}
+
+
+
+
+
+//const channel = interaction.options.getChannel('destination');
--- src/utils/slash/SlashDataUtil.js
+++ src/utils/slash/SlashDataUtil.js
@@ -8,6 +8,24 @@ export default class SlashDataUtil {
                 name: "deploy",
                 description: "Set all slash commands to the Ponjo Development guild.",
 
+            },
+            {
+                name: "poll",
+                description: "Create a poll for the guild.",
+                options: [
+                    {
+                        name: "channel",
+                        description: "The channel to send the poll in.",
+                        type: "CHANNEL",
+                        required: true,
+                    },
+                    {
+                        name: "description",
+                        description: "The context/description of the poll.",
+                        type: "STRING",
+                        required: true
+                    }
+                ]
             }];
     }
 }
