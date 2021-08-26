8fe58e900e8ef12f0287131c2ac130a75d287e7e
EerieAlchemist <66958528+EerieAlchemist@users.noreply.github.com>
2021-08-25 22:49:53 -0400

    added example config

---
+++ src/resources/Config.example.ts
@@ -0,0 +1,22 @@
+const config = {
+    token: "",
+    invite: "",
+    developer: {
+        owner: "",
+        ponjoGuild: "",
+        deploySlashCommands: false
+    },
+    ponjo_development: {
+        id: "",
+        verificationRole: "",
+        verificationChannel: "",
+        welcomeChannel: ""
+    },
+    emojis: {
+        success: "",
+        error: "",
+        loading: ""
+    }
+}
+
+export default config;
