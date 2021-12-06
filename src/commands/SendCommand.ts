3f913ae1ec3958886107e80d16475ef32fac1851
EerieAlchemist <66958528+EerieAlchemist@users.noreply.github.com>
2021-12-06 17:35:51 -0500

    added custom message component & permissions

--- src/commands/SendCommand.ts
+++ src/commands/SendCommand.ts
@@ -1,5 +1,5 @@
 import * as Discord from "discord.js";
-import {Client, MessageEmbed} from "discord.js";
+import {Client, MessageEmbed, Permissions} from "discord.js";
 import {ICommand} from "../structs/ICommand";
 import {SlashCommandOptions} from "../structs/ICommandOptions";
 import emojis from "../resources/Emojis";
@@ -22,8 +22,12 @@ export default class SendCommand implements ICommand {
     public async execute(interaction) {
         if (!interaction.isCommand()) return;
         if (interaction.commandName === this.name) {
+            if (!interaction.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
+                return await interaction.reply({content: "You don't have the required permissions to do this.", ephemeral: true});
+            }
             const component = interaction.options.getString("component");
             const channel = interaction.options.getChannel("channel");
+            const content = interaction.options.getString("content");
             let embed;
             switch (component) {
                 case "verification-message":
@@ -44,13 +48,13 @@ export default class SendCommand implements ICommand {
                     await interaction.reply({content: "The component has been sent successfully.", ephemeral: true});
                     return await channel.send({embeds: [embed], components: [row]});
                 case "custom-message":
-                    embed = new Discord.MessageEmbed()
-                        .setTitle("Welcome to the Ponjo Hangout!")
+                    embed = new MessageEmbed()
                         .setColor("#00e1ff")
-                        .setDescription("In order to join the server fully and gain access to all channels, please react to the message below. By reacting, you are agreeing to the server rules and to abide by Discord's Terms of Service. We hope you enjoy your stay!")
+                        .setDescription(content)
                         .setFooter("RoboEerie", this.client.user.displayAvatarURL({dynamic: true}))
                         .setTimestamp()
-                    break;
+                    await interaction.reply({content: "The component has been sent successfully.", ephemeral: true});
+                    return await channel.send({embeds: [embed]});
                 case "rules":
                     embed = new MessageEmbed()
                         .setTitle("Server Rules")
@@ -64,7 +68,7 @@ export default class SendCommand implements ICommand {
                             "7. **Do not violate Discord's Terms of Service.** You risk your own account by violating the [Discord TOS](https://discordapp.com/terms). We can’t do anything if you are terminated.\n" + "\n" +
                             "8. **Use bots in their corresponding channels.** Don’t use bots outside of their assigned channels.\n" + "\n" +
                             "9. **Don’t ping staff unless ABSOLUTELY necessary.** Randomly/spam pinging staff will result in a mute.\n" + "\n" +
-                            "10. **Have fun!** Enjoy your time in the Ponjo Development support server, and learn something new.")
+                            "10. **Have fun!** Enjoy your time in the server, and learn something new.")
                         .setFooter("RoboEerie", this.client.user.displayAvatarURL({dynamic: true}))
                         .setTimestamp()
                     break;
@@ -91,6 +95,10 @@ export default class SendCommand implements ICommand {
                     {
                         name: "Rules",
                         value: "rules"
+                    },
+                    {
+                        name: "Custom Message",
+                        value: "custom-message"
                     }
                 ]
             },
@@ -99,6 +107,12 @@ export default class SendCommand implements ICommand {
                 description: "The channel to send the component to.",
                 type: SlashCommandOptions.CHANNEL,
                 required: true
+            },
+            {
+                name: "content",
+                description: "The message content to send.",
+                type: SlashCommandOptions.STRING,
+                required: false
             }
         ]
     };
